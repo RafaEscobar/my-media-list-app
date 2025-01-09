b4f856f1b49094878c79ed0d3193dd11ab71ac04
rafa <rescobar@bit.lat>
2025-01-09 14:57:18 -0600

    Agrega focusNode a register form

--- lib/screens/auth/register_screen.dart
+++ lib/screens/auth/register_screen.dart
@@ -3,8 +3,27 @@ import 'package:mymedialist/widgets/general/button.dart';
 import 'package:mymedialist/widgets/general/input.dart';
 import 'package:mymedialist/widgets/general/label.dart';
 
-class RegisterScreen extends StatelessWidget {
+class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  final FocusNode _lastNFocusNode = FocusNode();
+  final FocusNode _emailFocusNode = FocusNode();
+  final FocusNode _passwordFocuesNode = FocusNode();
+  final FocusNode _nameFocusNode = FocusNode();
+
+  @override
+  void dispose() {
+    _nameFocusNode.dispose();
+    _lastNFocusNode.dispose();
+    _emailFocusNode.dispose();
+    _passwordFocuesNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,47 +32,53 @@ class RegisterScreen extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          const Column(
+          Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(height: 30,),
-              Label(text: 'Nombre:', size: 18,),
-              /*
+              const SizedBox(height: 30,),
+              const Label(text: 'Nombre:', size: 18,),
               Input(
+                name: 'name',
+                focusNode: _nameFocusNode,
                 obscureText: false,
                 keyboardType: TextInputType.text,
                 hintText: 'Raúl',
-                contentPadding: EdgeInsets.symmetric(vertical: 2),
+                contentPadding: const EdgeInsets.symmetric(vertical: 2),
                 maxLength: 60,
               ),
-              SizedBox(height: 30,),
-              Label(text: 'Apellidos:', size: 18,),
+              const SizedBox(height: 30,),
+              const Label(text: 'Apellidos:', size: 18,),
               Input(
+                name: 'last_name',
+                focusNode: _lastNFocusNode,
                 obscureText: false,
                 keyboardType: TextInputType.text,
                 hintText: 'Casillas',
-                contentPadding: EdgeInsets.symmetric(vertical: 2),
+                contentPadding: const EdgeInsets.symmetric(vertical: 2),
                 maxLength: 60,
               ),
-              SizedBox(height: 30,),
-              Label(text: 'Correo electrónico:', size: 18,),
+              const SizedBox(height: 30,),
+              const Label(text: 'Correo electrónico:', size: 18,),
               Input(
+                name: 'email',
+                focusNode: _emailFocusNode,
                 obscureText: false,
                 keyboardType: TextInputType.emailAddress,
                 hintText: 'example@mail.com',
-                contentPadding: EdgeInsets.symmetric(vertical: 2),
+                contentPadding: const EdgeInsets.symmetric(vertical: 2),
                 maxLength: 60,
               ),
-              SizedBox(height: 30,),
-              Label(text: 'Contraseña:', size: 18,),
+              const SizedBox(height: 30,),
+              const Label(text: 'Contraseña:', size: 18,),
               Input(
+                name: 'password',
+                focusNode: _passwordFocuesNode,
                 obscureText: true,
                 keyboardType: TextInputType.text,
                 hintText: '* * * * * * * *',
-                contentPadding: EdgeInsets.symmetric(vertical: 2),
+                contentPadding: const EdgeInsets.symmetric(vertical: 2),
                 maxLength: 16,
               ),
-              */
             ],
           ),
           Button(
