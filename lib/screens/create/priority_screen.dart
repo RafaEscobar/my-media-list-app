c58c8f6121d603501a62c53b94344bc1fb8794c8
rafa <rescobar@bit.lat>
2025-01-13 12:57:24 -0600

    Agrega screens de formularios create

---
+++ lib/screens/create/priority_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PriorityScreen extends StatelessWidget {
+  const PriorityScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/screens/create/type_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TypeScreen extends StatelessWidget {
+  const TypeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
