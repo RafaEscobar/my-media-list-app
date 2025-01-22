9f4948fb5f7787f183c0328270bd36bee9448412
rafa <rescobar@bit.lat>
2025-01-22 16:49:56 -0600

    Agrega logout local a función onUnauthorized

--- lib/utils/redirect.dart
+++ lib/utils/redirect.dart
@@ -1,7 +1,12 @@
 import 'package:go_router/go_router.dart';
 import 'package:mymedialist/main.dart';
+import 'package:mymedialist/provider/app_provider.dart';
 import 'package:mymedialist/screens/auth/auth_screen.dart';
+import 'package:provider/provider.dart';
 
 class Redirect {
-  static void onUnauthorized() => navigatorKey.currentState!.context.goNamed(AuthScreen.routeName);
+  static void onUnauthorized() {
+    navigatorKey.currentState!.context.read<AppProvider>().localLogout();
+    navigatorKey.currentState!.context.goNamed(AuthScreen.routeName);
+  }
 }
