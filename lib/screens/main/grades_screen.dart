333aad92c7f90b7245d57daa334adbec1e5edcab
rafa <rescobar@bit.lat>
2025-03-01 10:32:14 -0600

    Ajuste

--- lib/screens/main/grades_screen.dart
+++ lib/screens/main/grades_screen.dart
@@ -27,42 +27,6 @@ class _GradesScreenState extends State<GradesScreen> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Container(
-      padding: const EdgeInsets.only(left: 20, right: 20),
-      child: Stack(
-        children: [
-          Positioned(
-            top: 46,
-            right: 92,
-            left: 92,
-            child: CustomPaint(
-              size: Size(130, size.height * .4),
-              painter: Light()
-            ),
-          ),
-          const Positioned(
-            right: 0,
-            left: 0,
-            top: 180,
-            child: Podium()
-          ),
-          Positioned(
-            top: 350,
-            left: 0,
-            right: 0,
-            child: Container(
-              margin: const EdgeInsets.only(left: 10, right: 10),
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 126, 180, 247),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              width: double.infinity,
-              height: size.height * .5,
-              child: Placeholder()
-            ),
-          )
-        ],
-      ),
-    );
+    return 
   }
 }
---
+++ lib/widgets/structures/ranking_widget.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:mymedialist/models/entity.dart';
+import 'package:mymedialist/provider/ranking_provider.dart';
+import 'package:mymedialist/widgets/draws/light.dart';
+import 'package:mymedialist/widgets/draws/podium.dart';
+import 'package:provider/provider.dart';
+
+class RankingWidget extends StatelessWidget{
+  const RankingWidget({
+    super.key,
+    required this.currentList
+  });
+  final List<Entity> currentList;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      padding: const EdgeInsets.only(left: 20, right: 20),
+      child: Stack(
+        children: [
+          Positioned(
+            top: 46,
+            right: 92,
+            left: 92,
+            child: CustomPaint(
+              size: Size(130, size.height * .4),
+              painter: Light()
+            ),
+          ),
+          const Positioned(
+            right: 0,
+            left: 0,
+            top: 180,
+            child: Podium()
+          ),
+          Positioned(
+            top: 350,
+            left: 0,
+            right: 0,
+            child: Container(
+              margin: const EdgeInsets.only(left: 10, right: 10),
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 126, 180, 247),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              width: double.infinity,
+              height: size.height * .5,
+              child: Consumer<RankingProvider>(
+                builder: (context, value, child) {
+                  return ListView.builder(
+                    itemCount: currentList.length,
+                    itemBuilder: (context, index) => ,
+                  )
+                },
+              )
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
