3b91616bc2d52529ab38d7b9ebe65be799dd4f52
rafa <rescobar@bit.lat>
2025-03-03 09:19:47 -0600

    Agrega SingleChildScrollView

--- lib/widgets/structures/ranking_widget.dart
+++ lib/widgets/structures/ranking_widget.dart
@@ -22,95 +22,98 @@ class RankingWidget extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Container(
-      padding: const EdgeInsets.only(left: 20, right: 20),
-      child: Stack(
-        children: [
-          Positioned(
-            right: 0,
-            left: 0,
-            top: 60,
-            child: Row(
-              spacing: 50,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                if(shortList.length>=2) Container(
-                  margin: EdgeInsets.only(top: 35),
-                  child: Transform.rotate(
-                    angle: -0.24,
-                    child: ImageCard(imagePath: shortList[1].image, title: shortList[1].title)
-                  )
-                ),
-                if(shortList.isNotEmpty) Container(
-                  margin: EdgeInsets.only(bottom: 32),
-                  child: ImageCard(imagePath: shortList[0].image, title: shortList[0].title)
-                ),
-                if(shortList.length>=3) Container(
-                  margin: EdgeInsets.only(top: 38),
-                  child: Transform.rotate(
-                    angle: 0.24,
-                    child: ImageCard(imagePath: shortList[2].image, title: shortList[2].title)
-                  )
-                ),
-              ],
-            )
-          ),
-          Positioned(
-            top: 46,
-            right: 92,
-            left: 92,
-            child: CustomPaint(
-              size: Size(130, size.height * .4),
-              painter: Light()
-            ),
-          ),
-          const Positioned(
-            right: 0,
-            left: 0,
-            top: 180,
-            child: Podium()
-          ),
-          Positioned(
-            right: 0,
-            left: 0,
-            top: 200,
-            child: RankingPositions()
-          ),
-          Positioned(
-            top: 336,
-            left: 0,
-            right: 0,
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 8),
-              margin: const EdgeInsets.only(left: 14, right: 14),
-              decoration: BoxDecoration(
-                color: AppTheme.primary,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              width: double.infinity,
-              height: size.height * .5,
-              child: Consumer<RankingProvider>(
-                builder: (context, value, child) {
-                  if (list.isEmpty) {
-                    return const EmptyState(
-                    title: 'Aún no hay suficientes animes.',
-                    textStyle: TextStyle(color: Colors.white, fontSize: 22),
-                    lottieName: 'empty_state'
-                  );
-                  }
-                  return ListView.builder(
-                    itemCount: list.length,
-                    itemBuilder: (context, index) => RankingCard(
-                      entity: list[index],
-                      position: (index+4),
-                    ),
-                  );
-                },
+    return SingleChildScrollView(
+      child: Container(
+        height: size.height * .94,
+        padding: const EdgeInsets.only(left: 20, right: 20),
+        child: Stack(
+          children: [
+            Positioned(
+              right: 0,
+              left: 0,
+              top: 60,
+              child: Row(
+                spacing: 50,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  if(shortList.length>=2) Container(
+                    margin: EdgeInsets.only(top: 35),
+                    child: Transform.rotate(
+                      angle: -0.24,
+                      child: ImageCard(imagePath: shortList[1].image, title: shortList[1].title)
+                    )
+                  ),
+                  if(shortList.isNotEmpty) Container(
+                    margin: EdgeInsets.only(bottom: 32),
+                    child: ImageCard(imagePath: shortList[0].image, title: shortList[0].title)
+                  ),
+                  if(shortList.length>=3) Container(
+                    margin: EdgeInsets.only(top: 38),
+                    child: Transform.rotate(
+                      angle: 0.24,
+                      child: ImageCard(imagePath: shortList[2].image, title: shortList[2].title)
+                    )
+                  ),
+                ],
               )
             ),
-          )
-        ],
+            Positioned(
+              top: 46,
+              right: 92,
+              left: 92,
+              child: CustomPaint(
+                size: Size(130, size.height * .4),
+                painter: Light()
+              ),
+            ),
+            const Positioned(
+              right: 0,
+              left: 0,
+              top: 180,
+              child: Podium()
+            ),
+            Positioned(
+              right: 0,
+              left: 0,
+              top: 200,
+              child: RankingPositions()
+            ),
+            Positioned(
+              top: 336,
+              left: 0,
+              right: 0,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+                margin: const EdgeInsets.only(left: 14, right: 14),
+                decoration: BoxDecoration(
+                  color: AppTheme.primary,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                width: double.infinity,
+                height: size.height * .5,
+                child: Consumer<RankingProvider>(
+                  builder: (context, value, child) {
+                    if (list.isEmpty) {
+                      return const EmptyState(
+                      title: 'Aún no hay suficientes animes.',
+                      textStyle: TextStyle(color: Colors.white, fontSize: 22),
+                      lottieName: 'empty_state'
+                    );
+                    }
+                    return ListView.builder(
+                      itemCount: list.length,
+                      itemBuilder: (context, index) => RankingCard(
+                        entity: list[index],
+                        position: (index+4),
+                      ),
+                    );
+                  },
+                )
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
